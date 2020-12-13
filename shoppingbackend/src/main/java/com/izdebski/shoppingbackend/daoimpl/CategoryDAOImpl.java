26ffaa8d3fa50834a47d4613ea196fd20d88ad97
SmartSoft <iizdepskyy112@gmail.com>
2020-12-13 23:25:22 +0200

    03_08_crud_operation_junit_test_case

--- shoppingbackend/src/main/java/com/izdebski/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/src/main/java/com/izdebski/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -1,87 +1,91 @@
 package com.izdebski.shoppingbackend.daoimpl;
 
+import java.util.List;
+
 import com.izdebski.shoppingbackend.dao.CategoryDAO;
 import com.izdebski.shoppingbackend.dto.Category;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
 
     @Autowired
     private SessionFactory sessionFactory;
 
+    @Override
+    public List<Category> list() {
 
-    private static List<Category> categories = new ArrayList<>();
+        String selectActiveCategory = "FROM Category WHERE active = :active";
 
-    static {
+        Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
 
-        Category category = new Category();
+        query.setParameter("active", true);
 
-        // adding first category
-        category.setId(1);
-        category.setName("Television");
-        category.setDescription("This is some description for television");
-        category.setImageURL("CAT_1.png");
+        return query.getResultList();
+    }
 
-        categories.add(category);
+    /*
+     * Getting single category based on id
+     */
+    @Override
+    public Category get(int id) {
 
-        // second category
-        category = new Category();
-        category.setId(2);
-        category.setName("Mobile");
-        category.setDescription("This is some description for mobile");
-        category.setImageURL("CAT_2.png");
-
-        categories.add(category);
-
-        // third category
-        category = new Category();
-        category.setId(3);
-        category.setName("Laptop");
-        category.setDescription("This is some description for laptop");
-        category.setImageURL("CAT_3.png");
-
-        categories.add(category);
-
+        return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 
     }
 
     @Override
-    @Transactional
+
     public boolean add(Category category) {
 
         try {
             // add the category to the database table
             sessionFactory.getCurrentSession().persist(category);
-
             return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
         }
-        catch (Exception ex) {
+
+    }
+
+    /*
+     * Updating a single category
+     */
+    @Override
+    public boolean update(Category category) {
+
+        try {
+            // add the category to the database table
+            sessionFactory.getCurrentSession().update(category);
+            return true;
+        } catch (Exception ex) {
             ex.printStackTrace();
             return false;
         }
     }
 
     @Override
-    public List<Category> list() {
-        return categories;
-    }
+    public boolean delete(Category category) {
 
-    @Override
-    public Category get(int id) {
+        category.setActive(false);
 
-        // enhanced for loop
-        for (Category category : categories) {
-
-            if (category.getId() == id)
-                return category;
+        try {
+            // add the category to the database table
+            sessionFactory.getCurrentSession().update(category);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
         }
-        return null;
     }
+
 }
+
