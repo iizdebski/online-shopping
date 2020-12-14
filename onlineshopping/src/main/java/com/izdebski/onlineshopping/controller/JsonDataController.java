f89fbe522ccff5d514917101d2fff502bc970af3
SmartSoft <iizdepskyy112@gmail.com>
2020-12-14 15:45:03 +0200

    04_02_getting_product_data_in_json_format_spring_mvc

---
+++ onlineshopping/src/main/java/com/izdebski/onlineshopping/controller/JsonDataController.java
@@ -0,0 +1,33 @@
+package com.izdebski.onlineshopping.controller;
+
+import com.izdebski.shoppingbackend.dao.ProductDAO;
+import com.izdebski.shoppingbackend.dto.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/json/data")
+public class JsonDataController {
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    @RequestMapping("/all/products")
+    @ResponseBody
+    public List<Product> getAllProducts() {
+
+        return productDAO.listActiveProducts();
+    }
+
+    @RequestMapping("/category/{id}/products")
+    @ResponseBody
+    public List<Product> getProductsByCategory(@PathVariable int id) {
+
+        return productDAO.listActiveProductsByCategory(id);
+    }
+}
