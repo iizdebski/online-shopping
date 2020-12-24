b295ffd6ab287f91d893bb9ea412953eff3e9a29
SmartSoft <iizdepskyy112@gmail.com>
2020-12-24 13:58:01 +0200

    07_05_role_based_access_control

--- onlineshopping/src/main/java/com/izdebski/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/izdebski/onlineshopping/controller/PageController.java
@@ -135,4 +135,14 @@ public class PageController {
         mv.addObject("title", "Login");
         return mv;
     }
+
+    /* access denied page */
+    @RequestMapping(value = "/access-denied")
+    public ModelAndView accessDenied() {
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("title", "403 - Access Denied");
+        mv.addObject("errorTitle", "Aha! Caught You.");
+        mv.addObject("errorDescription", "You are not authorized to view this page");
+        return mv;
+    }
 }
